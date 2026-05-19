d18128633a190f4578c2224163aa1e49d0d853a9
lyqgzbl <122811297+lyqgzbl@users.noreply.github.com>
2026-05-19 08:52:02 +0800

    fix

--- Module/QQ_Redirect/qq_redirect.js
+++ Module/QQ_Redirect/qq_redirect.js
@@ -1,18 +1,36 @@
-var redirectRules = [
-  /^https:\/\/c\.pc\.qq\.com\/middlem\.html\?pfurl=(http.*)(&pfuin=.*)/,
-  /^https:\/\/c\.pc\.qq\.com\/middlect\.html\?pfuin=.*&pfurl=(http.*)(&gjsublevel=.*)/,
-  /^https:\/\/c\.pc\.qq\.com\/middlect\.html\?iscontinue=.*pfurl=(http.*)(&pfuin=.*)/,
-  /^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/middlem\.html\?pfurl%3d(http.*)(%26pfuin%3d.*%26pfuin%3d.*)/,
-  /^https:\/\/cgi\.connect\.qq\.com\/qqconnectopen\/get_urlinfoForQQV2\?url=(http.*)/,
-  /^https:\/\/c\.pc\.qq\.com\/index\.html\?pfurl=(http.*)(&pfuin=.*)/,
-  /^https:\/\/c\.pc\.qq\.com\/ios\.html\?url=(http.*)(&level=.*)/,
-  /^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/ios\.html\?url%3d(http.*)(%26level.*%26level.*)/,
-];
+function safeDecode(value) {
+  var decoded = value;
 
+  for (var i = 0; i < 3; i++) {
+    try {
+      var next = decodeURIComponent(decoded);
+      if (next === decoded) {
+        break;
+      }
+      decoded = next;
+    } catch (e) {
+      break;
+    }
+  }
+
+  return decoded;
+}
+
+var decodedUrl = safeDecode($request.url);
+var redirectRules = [
+  /^https:\/\/c\.pc\.qq\.com\/middlem\.html\?pfurl=(https?:\/\/.*)(&pfuin=.*)/,
+  /^https:\/\/c\.pc\.qq\.com\/middlect\.html\?pfuin=.*&pfurl=(https?:\/\/.*)(&gjsublevel=.*)/,
+  /^https:\/\/c\.pc\.qq\.com\/middlect\.html\?iscontinue=.*pfurl=(https?:\/\/.*)(&pfuin=.*)/,
+  /^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/middlem\.html\?pfurl=(https?:\/\/.*)(&pfuin=.*&pfuin=.*)/,
+  /^https:\/\/cgi\.connect\.qq\.com\/qqconnectopen\/get_urlinfoForQQV2\?url=(https?:\/\/.*)/,
+  /^https:\/\/c\.pc\.qq\.com\/index\.html\?pfurl=(https?:\/\/.*)(&pfuin=.*)/,
+  /^https:\/\/c\.pc\.qq\.com\/ios\.html\?url=(https?:\/\/.*)(&level=.*)/,
+  /^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/ios\.html\?url=(https?:\/\/.*)(&level.*&level.*)/,
+];
 var targetUrl = null;
 
 for (var i = 0; i < redirectRules.length; i++) {
-  var matched = $request.url.match(redirectRules[i]);
+  var matched = decodedUrl.match(redirectRules[i]);
   if (matched) {
     targetUrl = matched[1];
     break;
